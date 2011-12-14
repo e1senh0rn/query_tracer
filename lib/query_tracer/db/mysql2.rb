5363fb789b55b87b70be469b8cd90500750ab3a5
Dmitry Shaposhnik <dmitry@shaposhnik.name>
2011-12-14 17:29:43 +0200

    Due to adapter name changes corresponding modules added.

---
+++ lib/query_tracer/db/mysql2.rb
@@ -0,0 +1,7 @@
+module QueryTracer
+  module Db
+    module Mysql2
+      include QueryTracer::Db::Mysql
+    end
+  end
+end
---
+++ lib/query_tracer/db/sqlite3.rb
@@ -0,0 +1,7 @@
+module QueryTracer
+  module Db
+    module Sqlite3
+      include QueryTracer::Db::Sqlite
+    end
+  end
+end
